ecc67f463061259594ee8d8ec9bf99defce22d95
yangyxu <yangyxu@cisco.com>
2021-04-24 23:47:14 +0800

    add isWeChatClient

--- src/znui.js
+++ src/znui.js
@@ -137,6 +137,15 @@ module.exports = window.znui = {
         document.body.removeChild(_tempInput);
         callback && callback();
 	},
+    isWeChatClient: function () {
+        var _ua = window.navigator.userAgent.toLowerCase();
+        //mozilla/5.0 (iphone; cpu iphone os 9_1 like mac os x) applewebkit/601.1.46 (khtml, like gecko)version/9.0 mobile/13b143 safari/601.1
+        if (_ua.match(/MicroMessenger/i) == 'micromessenger') {
+            return true;
+        } else {
+            return false;
+        }
+    },
     isAndroid: function (){
         return navigator.userAgent.indexOf('Android') > -1 || navigator.userAgent.indexOf('Adr') > -1;
     },
